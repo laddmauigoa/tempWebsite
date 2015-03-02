f859d34d15a312925e09c4829b05e043c27faaa1
ladd mauigoa <laddmauigoa@gmail.com>
2015-03-02 09:11:06 -0700

    plz work

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.listen(process.env.EXPRESS_PORT || 8888 function() {
+app.listen(process.env.EXPRESS_PORT || 8888, function() {
 	console.log('listening on ' + port)
 })
