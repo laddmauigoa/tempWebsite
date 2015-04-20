acc29302e6a84ed4559135ec36e388a26ae43e8d
ladd mauigoa <laddmauigoa@gmail.com>
2015-04-19 22:04:07 -0600

    changed the jpg to lowercase and cleaned up server.js

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
-var port = 80;
+
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.listen(process.env.EXPRESS_PORT || 8888, function() {
-	console.log('listening on ' + port)
-})
+app.listen(process.env.EXPRESS_PORT || 8888);
